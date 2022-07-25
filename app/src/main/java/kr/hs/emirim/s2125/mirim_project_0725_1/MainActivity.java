951959a34e1fa2d30838918ed4ea251892e97f84
seungchae1 <s2125@e-mirim.hs.kr>
2022-07-25 11:10:05 +0900

    tab_error

--- app/src/main/java/kr/hs/emirim/s2125/mirim_project_0725_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2125/mirim_project_0725_1/MainActivity.java
@@ -1,14 +1,24 @@
 package kr.hs.emirim.s2125.mirim_project_0725_1;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity implements ActionBar.TabListener {
 
     ActionBar.Tab tab1, tab2, tab3;
+    MyFragment[] myFrags = new MyFragment[3];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +43,17 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
 
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-
+        MyFragment selectedFragment = null;
+        if(myFrags[tab.getPosition()]==null){
+            selectedFragment = new MyFragment();
+            Bundle data = new Bundle();
+            data.putString("tabName", tab.getText().toString());
+            selectedFragment.setArguments(data);
+            myFrags[tab.getPosition()] = selectedFragment;
+        }else{
+            selectedFragment = myFrags[tab.getPosition()];
+        }
+        ft.replace(android.R.id.content, selectedFragment);
     }
 
     @Override
@@ -44,5 +64,33 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
     @Override
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
 
+    }
+    public class MyFragment extends Fragment{
+        String tabName;
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            Bundle data = getArguments();
+            tabName = data.getString("tabName");
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            LinearLayout layout = new LinearLayout(super.getActivity());
+            layout.setOrientation(LinearLayout.VERTICAL);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            layout.setLayoutParams(params);
+            if(tabName.equals("변백현")){
+                layout.setBackgroundColor(Color.BLUE);
+            }
+            else if(tabName.equals("오세훈")){
+                layout.setBackgroundColor(Color.YELLOW);
+            }
+            else if(tabName.equals("김준면")){
+                layout.setBackgroundColor(Color.MAGENTA);
+            }
+            return layout;
+        }
     }
 }
